0d6bee7cebbb559b3d794afbc4ff1145fc7ac1a0
felipedcp20 <felipedcp@gmail.com>
2022-11-05 22:41:43 -0500

    create env and connection to mongo db

---
+++ app/functions/databaseMongo.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from fastapi import HTTPException
+
+
+def connection_dbMongo(url, username, password):
+    """_summary_
+
+    Args:
+        url (str): _description_
+        username (str): _description_
+        password (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    url = url.replace("<user>", username).replace("<password>", password)
+
+    client = MongoClient(url)
+
+    try:
+        client.server_info()
+    except Exception as error:
+        detail = f"connection to sql: ok , please consult the mongoAcces {error}"
+        raise HTTPException(status_code=404, detail= detail) from error
+
+    return client
